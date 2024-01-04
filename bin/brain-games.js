2c96274785d1067ceb48278d925525e266d874b0
AdelVa <radioburst@bk.ru>
2024-01-04 16:16:28 +0300

    Step 4. eslint fix brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-import AskNameGreet from "../src/cli.js";
-console.log("Welcome to the Brain Games!");
+import { AskNameGreet } from '../src/cli.js';
+
+console.log('Welcome to the Brain Games!');
 AskNameGreet();
 process.exit(1);
