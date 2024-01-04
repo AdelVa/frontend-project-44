143b7f0e821206cb0d0b4d4c52f870a9509f8eb2
AdelVa <radioburst@bk.ru>
2024-01-04 16:15:56 +0300

    Step 4. eslint fix cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
-import readlineSync from "../node_modules/readline-sync/lib/readline-sync.js";
+import readlineSync from 'readline-sync/lib/readline-sync.js';
 
 export const AskNameGreet = () => {
-  var userName = readlineSync.question("May I have your name? ");
-  return console.log("Hello, " + userName + "!");
+  const userName = readlineSync.question('May I have your name? ');
+  return console.log(`Hello, ${userName}!`);
 };
 
 export default AskNameGreet;
