fb66ebde25e364ad702e31a9e67d1ce300ed6d8f
AdelVa <radioburst@bk.ru>
2024-01-10 12:56:41 +0300

    Step 5. bin/brain-even.js updated+lint-fixed

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import { brainGameEven } from "../src/bg-even-logic.js";
+import { brainGameEven } from '../src/bg-even-logic.js';
 
-console.log("Welcome to the Brain Games!");
+console.log('Welcome to the Brain Games!');
 brainGameEven();
 process.exit(1);
