f59477e31f9b728be25a42606b7e4794046aa6db
AdelVa <radioburst@bk.ru>
2024-02-13 15:55:48 +0300

    Step 6. linted bin/brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import { brainGameCalc } from "../src/bg-calc-logic.js";
+import { brainGameCalc } from '../src/bg-calc-logic.js';
 
-console.log("Welcome to the Brain Games!");
+console.log('Welcome to the Brain Games!');
 brainGameCalc();
 process.exit(1);
