eef215c7de4aba617f39523b01cf3d590ffb4fc9
AdelVa <radioburst@bk.ru>
2024-02-23 15:29:23 +0300

    unified game engine located in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import readlineSync from 'readline-sync/lib/readline-sync.js';
+
 const giveMeRand = (max) => Math.floor(Math.random() * (max - 1 + 1)) + 1;
 
 const checkAnswer = (userAnswer, gameAnswer) => !(userAnswer === gameAnswer.toString());
@@ -10,6 +12,21 @@ const gameEndMessage = (userName, gameError) => (gameError === false
   ? console.log(`Congratulations, ${userName}!`)
   : console.log(`Let's try again, ${userName}!`));
 
-export {
-  giveMeRand, checkAnswer, checkMessage, gameEndMessage,
+const gameEngine = (gameRule, gameValues) => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(gameRule);
+  let i = 0;
+  let gameError = false;
+  while (i < 3 && gameError === false) {
+    const [gameQuestion, gameAnswer] = gameValues();
+    i += 1;
+    console.log(`Question: ${gameQuestion}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    gameError = checkAnswer(userAnswer, gameAnswer);
+    console.log(checkMessage(gameError, userAnswer, gameAnswer));
+  }
+  return gameEndMessage(userName, gameError);
 };
+
+export { giveMeRand, gameEngine };
